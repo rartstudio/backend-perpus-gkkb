5dece31c8550b1c41ebf43336f8a746de1d32eef
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-08-25 22:24:02 +0700

    menambahkan fitur query pada books api check if pada index

--- app/Http/Controllers/Api/BooksController.php
+++ app/Http/Controllers/Api/BooksController.php
@@ -2,19 +2,52 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Authors;
 use App\Books;
+use App\CategoriesBook;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Books\BookCollection;
 use App\Http\Resources\Books\BookResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BooksController extends Controller
 {
-    public function index() 
+    public function index(Request $request) 
     {
-        //get all data book
-        $data = Books::with(['author','categories_book','publisher','review'])->paginate(5);
+        //query name
+        if ($request->query('name')){
+            //get query
+            $query = $request->query('name');    
+            //get all data book
+            $data = Books::with(['author','categories_book','publisher','review'])->where('title','like','%'.$query.'%')->get();   
+        }
+        else if ($request->query('author')){
+            //get query
+            $query = $request->query('author');    
+            
+            //get id of author
+            $id = Authors::select('id')->where('author_name','like','%'.$query.'%')->get();
 
+            //encode it to array associative
+            $parser = json_decode($id);
+
+            //a container for using where in
+            $conArray = array();
+
+            //loop through array associative and object inside
+            foreach($parser as $value){
+                $int = $value->id;
+                array_push($conArray,$int);
+            }
+
+            //get data
+            $data = Books::with(['author','categories_book','publisher','review'])->whereIn('author_id',$conArray)->get();
+        }
+        else {
+            $data = Books::with(['author','categories_book','publisher','review'])->take(10)->get();
+        }
+        
         return new BookCollection($data);
     }
 
