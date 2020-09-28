57cc80470d72a7413901b9c661c4c20bb082c7f9
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-09-28 12:03:26 +0700

    menambah kolom berapa hari tersisa di transaction resource

--- app/Http/Resources/Transaction/TransactionResource.php
+++ app/Http/Resources/Transaction/TransactionResource.php
@@ -16,17 +16,25 @@ class TransactionResource extends JsonResource
      */
     public function toArray($request)
     {
+
+        $returned_at = 0;
+
+        if(is_null($returned_at) != 1){
+            $returned_at = Carbon::parse($this->returned_at)->diffInDays();
+        }
+
         return [
             'id' => $this->id,
             'transaction_code' => $this->transaction_code,
             'stated' => $this->state,
             'qty_borrow' => $this->qty_borrow,
             'qty_returned' => $this->qty_returned,
-            'returned_at' => $this->returned_at,
             'add_info' => $this->add_info,
-            'borrowed_at' => $this->borrowed_at,
+            'borrowed_at' => Carbon::parse($this->borrowed_at)->format('d M Y H:i:s'),
+            'returned_at' => Carbon::parse($this->returned_at)->format('d M Y H:i:s'),
             'created_at' => Carbon::parse($this->created_at)->format('d M Y H:i:s'),
             'transaction_details' => TransactionDetailResource::collection($this->whenLoaded('transaction_details')),
+            'is_late' => $returned_at.' '.'hari'
             // 'transaction_details' => 1
         ];
     }
